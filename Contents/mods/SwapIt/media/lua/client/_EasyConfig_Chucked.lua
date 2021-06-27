d09f524409c9e836cf674a7b49b27793450dfa43
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-06-27 11:11:33 -0400

    Update _EasyConfig_Chucked.lua

--- Contents/mods/SwapIt/media/lua/client/_EasyConfig_Chucked.lua
+++ Contents/mods/SwapIt/media/lua/client/_EasyConfig_Chucked.lua
@@ -11,7 +11,7 @@ EasyConfig_Chucked.addMod = function(modId, name, config, configMenu, tabName, m
 	EasyConfig_Chucked.mods[modId].name = name
 	EasyConfig_Chucked.mods[modId].config = config
 	EasyConfig_Chucked.mods[modId].configMenu = configMenu
-	EasyConfig_Chucked.mods[modId].tabName = tabName or "MODS"
+	EasyConfig_Chucked.mods[modId].tabName = tabName
 	EasyConfig_Chucked.mods[modId].menuSpecificAccess = menuSpecificAccess
 
 	--link all the things!
@@ -240,97 +240,29 @@ function MainOptions:create() -- override
 		self.addY = self.addY + 30
 	end
 
-	local modPageAdded = false
 	for modId,mod in pairs(EasyConfig_Chucked.mods) do
-		if (not mod.menuSpecificAccess) or (getPlayer() and mod.menuSpecificAccess=="ingame") or (not getPlayer() and mod.menuSpecificAccess=="mainmenu") then
-			if mod.tabName == "MODS" then
-				if not modPageAdded then
-					self:addPage("MODS")
-					modPageAdded = true
-				end
-				createElements(mod)
-			end
-		end
-	end
-	self.addY = self.addY + MainOptions.translatorPane:getHeight() + 22
-	self.mainPanel:setScrollHeight(self.addY + 20)
 
-	for modId,mod in pairs(EasyConfig_Chucked.mods) do
-		if (not mod.menuSpecificAccess) or (getPlayer() and mod.menuSpecificAccess=="ingame") or (not getPlayer() and mod.menuSpecificAccess=="mainmenu") then
-			if mod.tabName ~= "MODS" then
-				self:addPage(mod.tabName)
-				self.addY = 0
-				createElements(mod)
-				self.addY = self.addY + MainOptions.translatorPane:getHeight() + 22
-				self.mainPanel:setScrollHeight(self.addY + 20)
-			end
-		end
-	end
-end
-
-
-
-EasyConfig_Chucked.saveConfig = function()
-	for modId,mod in pairs(EasyConfig_Chucked.mods) do
-		local config = mod.config
-		local configMenu = mod.configMenu
+		self.addY = 0
+		self:addPage(mod.tabName)
 
 		if (not mod.menuSpecificAccess) or (getPlayer() and mod.menuSpecificAccess=="ingame") or (not getPlayer() and mod.menuSpecificAccess=="mainmenu") then
-			local configFile = "media/config/"..modId..".config"
-			local fileWriter = getModFileWriter(modId, configFile, true, false)
-			if fileWriter then
-				print("modId: "..modId.." saving")
-				for gameOptionName,_ in pairs(config) do
-					local menuEntry = configMenu[gameOptionName]
-
-					if menuEntry.selectedLabel then
-						local menuEntry_selectedLabel = menuEntry.selectedLabel
-						if type(menuEntry.selectedLabel) == "boolean" then
-							menuEntry_selectedLabel = tostring(menuEntry_selectedLabel)
-						end
-						fileWriter:write(gameOptionName.."="..menuEntry_selectedLabel..",\r")
-					else
-						local menuEntry_selectedValue = menuEntry.selectedValue
-						if type(menuEntry.selectedValue) == "boolean" then
-							menuEntry_selectedValue = tostring(menuEntry_selectedValue)
-						end
-						fileWriter:write(gameOptionName.."="..menuEntry_selectedValue..",\r")
-					end
-				end
-				fileWriter:close()
+			createElements(mod)
+		else
+			if (not getPlayer() and mod.menuSpecificAccess=="ingame") then
+				addText("This mod's options can only be accessed", UIFont.Medium)
+				addText("from the in-game options menu.", UIFont.Medium)
+			end
+			if (getPlayer() and mod.menuSpecificAccess=="mainmenu") then
+				addText("This mod's options can only be", UIFont.Medium)
+				addText("accessed from the main menu options menu.", UIFont.Medium)
+				addText(" ", UIFont.Medium)
+				addText("Note: Make sure to enable this mod", UIFont.Medium)
+				addText("from the main menu to view the options.", UIFont.Medium)
 			end
 		end
-	end
-end
-EasyConfig_Chucked.loadConfig = function()
-	for modId,mod in pairs(EasyConfig_Chucked.mods) do
-		local config = EasyConfig_Chucked.mods[modId].config
-		local configMenu = EasyConfig_Chucked.mods[modId].configMenu
-		local configFile = "media/config/"..modId..".config"
-		local fileReader = getModFileReader(modId, configFile, false)
-		if fileReader then
-			print("modId: "..modId.." loading")
-			for _,_ in pairs(config) do
-				local line = fileReader:readLine()
-				if not line then break end
-				for gameOptionName,label in string.gmatch(line, "([^=]*)=([^=]*),") do
-					local menuEntry = configMenu[gameOptionName]
-					if menuEntry then
 
-						if menuEntry.options then
-							menuEntry.selectedIndex = menuEntry.optionsKeys[label][1]
-							menuEntry.selectedValue = menuEntry.optionsKeys[label][2]
-							menuEntry.selectedLabel = label
-						else
-							if label == "true" then menuEntry.selectedValue = true
-							elseif label == "false" then menuEntry.selectedValue = false
-							else menuEntry.selectedValue = tonumber(label) end
-						end
-						config[gameOptionName] = menuEntry.selectedValue
-					end
-				end
-			end
-			fileReader:close()
-		end
+		self.addY = self.addY + MainOptions.translatorPane:getHeight() + 22
+		self.mainPanel:setScrollHeight(self.addY + 20)
+
 	end
 end
